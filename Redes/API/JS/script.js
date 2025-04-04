ea3d16dfa69848dd69f5d61a15dc16b31e8a4926
Pablo Vinicius <128602954+PabloVLS@users.noreply.github.com>
2025-04-04 17:00:55 -0300

    Add files via upload

---
+++ Redes/API/JS/script.js
@@ -0,0 +1,14 @@
+// scripts.js
+
+document.addEventListener("DOMContentLoaded", function () {
+    const seletor = document.querySelector("select");
+    
+    if (seletor) {
+        seletor.addEventListener("change", function () {
+            const url = this.value;
+            if (url) {
+                window.location.href = url;
+            }
+        });
+    }
+});
